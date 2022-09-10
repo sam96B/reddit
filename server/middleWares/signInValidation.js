a170b297667cba497bca2e50844c6578418dd7ba
Sami A Ba‘alousha <same-96@hotmail.com>
2022-09-10 23:47:39 +0300

    Update signInValidation.js

--- server/middleWares/signInValidation.js
+++ server/middleWares/signInValidation.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const { singinSchema, DataBaseError, comparePasswords } = require('../Utills');
 const { getUserByEmailQuery, getUserByIdQuery } = require('../database/queries');
 
